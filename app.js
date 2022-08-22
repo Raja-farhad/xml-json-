357a0a0db086a160b762f24baeb4b979e8544069
muhammad farhad <108810825+Raja-farhad@users.noreply.github.com>
2022-08-22 06:24:14 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name").innerHTML = data.record[0].name);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name1").innerHTML = data.record[0].email);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name2").innerHTML = data.record[0].phone);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name3").innerHTML = data.record[0].semester);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name4").innerHTML = data.record[0].job);
+
+
+
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name5").innerHTML = data.record[1].name);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name6").innerHTML = data.record[1].email);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name7").innerHTML = data.record[1].phone);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name8").innerHTML = data.record[1].semester);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name9").innerHTML = data.record[1].job);
+
+
+
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name10").innerHTML = data.record[2].name);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name11").innerHTML = data.record[2].email);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name12").innerHTML = data.record[2].phone);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name13").innerHTML = data.record[2].semester);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name14").innerHTML = data.record[2].job);
+
+
+
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name15").innerHTML = data.record[3].name);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name16").innerHTML = data.record[3].email);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name17").innerHTML = data.record[3].phone);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name18").innerHTML = data.record[3].semester);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name19").innerHTML = data.record[3].job);
+
+
+
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name20").innerHTML = data.record[4].name);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name21").innerHTML = data.record[4].email);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name22").innerHTML = data.record[4].phone);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name23").innerHTML = data.record[4].semester);
+fetch("data.json").then(res => res.json()).then(data => document.getElementById("name24").innerHTML = data.record[4].job);
